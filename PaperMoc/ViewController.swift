a3d7606f255626e2426c28262a2357ce8b5383e6
MegumuIsshiki <m.isshiki0821@icloud.com>
2019-12-20 15:34:24 +0900

    Initial Commit

---
+++ PaperMoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaperMoc
+//
+//  Created by 一色潤 on 2019/12/20.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
