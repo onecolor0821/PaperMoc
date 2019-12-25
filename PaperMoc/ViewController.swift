2763f3dd4875cbc7d7ba54d544563c347e8ab3d9
MegumuIsshiki <m.isshiki0821@gmail.com>
2019-12-25 20:32:16 +0900

    UI作成、カメラ機能

--- PaperMoc/ViewController.swift
+++ PaperMoc/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func startCamera(_ sender: UIBarButtonItem) {
+        
+        //UIImagePickerController起動時にカメラの使用の宣言。一個目のsourceTypeは任意(以降は定型文)。
+        let sourceType:UIImagePickerController.SourceType = UIImagePickerController.SourceType.camera
+
+        //起動時にカメラが使えるかどうか
+        if
+        /*UIImagePickerを操作*/ UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
